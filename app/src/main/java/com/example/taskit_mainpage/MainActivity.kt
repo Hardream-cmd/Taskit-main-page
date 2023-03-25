77e01bee0270fe6f71cff21280c7b8284e148e4f
Arnaud <arnaud.a.Durand@gmail.com>
2023-03-25 13:55:00 +0200

    v2

--- app/src/main/java/com/example/taskit_mainpage/MainActivity.kt
+++ app/src/main/java/com/example/taskit_mainpage/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -12,7 +11,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
@@ -92,38 +93,51 @@ fun TwoButtonRow(){
 
 @Composable
 fun DropdownList(){
-    val options = listOf("Option 1", "Option 2", "Option 3")
-    var expanded by remember { mutableStateOf(false) }
-    var selectedIndex by remember { mutableStateOf(0) }
+    val items = listOf("Option 1", "Option 2", "Option 3")
+    val (expanded, setExpanded) = remember { mutableStateOf(false) }
+    val selectedIndex = remember { mutableStateOf(0) }
 
-    Column {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "Select an option:",
+            style = MaterialTheme.typography.subtitle1,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .clickable(onClick = { expanded = !expanded })
-                .background(androidx.compose.ui.graphics.Color.Gray)
+                .border(1.dp, MaterialTheme.colors.onSurface, shape = RoundedCornerShape(4.dp))
+                .clickable(onClick = { setExpanded(!expanded) })
         ) {
-            Text(
-                text = options[selectedIndex],
-                modifier = Modifier.padding(16.dp),
-                color = androidx.compose.ui.graphics.Color.White
-            )
-            Icon(
-                imageVector = Icons.Filled.ArrowDropDown,
-                contentDescription = null,
-                modifier = Modifier.align(Alignment.CenterEnd)
-            )
+            Row(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    text = items[selectedIndex.value],
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.weight(1f)
+                )
+                Icon(
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    contentDescription = "Dropdown Icon"
+                )
+            }
         }
         DropdownMenu(
             expanded = expanded,
-            onDismissRequest = { expanded = false }
+            onDismissRequest = { setExpanded(false) },
+            modifier = Modifier.fillMaxWidth()
         ) {
-            options.forEachIndexed { index, option ->
-                DropdownMenuItem(onClick = {
-                    selectedIndex = index
-                    expanded = false
-                }) {
-                    Text(text = option)
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(
+                    onClick = {
+                        selectedIndex.value = index
+                        setExpanded(false)
+                    }
+                ) {
+                    Text(text = text)
                 }
             }
         }
