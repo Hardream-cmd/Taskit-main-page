1f06fa0f69c19e3842dbc9082fbac97c50ad76de
Arnaud <arnaud.a.Durand@gmail.com>
2023-03-25 16:33:21 +0200

    v3

--- app/src/main/java/com/example/taskit_mainpage/MainActivity.kt
+++ app/src/main/java/com/example/taskit_mainpage/MainActivity.kt
@@ -10,20 +10,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.taskit_mainpage.ui.theme.TaskitmainpageTheme
 import androidx.compose.ui.unit.dp
-
+import com.example.taskit_mainpage.ui.theme.TaskitmainpageTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,29 +38,76 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun MyApp(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "Home"
+    ){
+        composable(route="Home"){
+            MainScreen(navController)
+        }
+        composable(route = "info"){
+            InfoScreen(navController = navController)
+        }
+        composable(route="Settings"){
+            SettingsScreen(navController)
+        }
+    }
+}
+
+@Composable
+fun MainTopBar(title: String, navController: NavController){
+    var expanded by remember { mutableStateOf(false)}
+    TopAppBar(
+        title = { Text(title) },
+        actions = {
+            IconButton(
+                onClick = {
+                    expanded = !expanded
+                }
+            ){
+                Icon(Icons.Filled.MoreVert, contentDescription = null)
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }){
+                DropdownMenuItem(onClick = { navController.navigate("info") }) {
+                    Text("Info")
+                }
+                DropdownMenuItem(onClick = { navController.navigate("settings") }) {
+                    Text("Settings")
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun ScreenTopBar(title: String, navController: NavController){
+    TopAppBar (
+        title={Text(title)},
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp()}){
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        }
+    )
+}
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun MyApp() {
+fun MainScreen(navController: NavController){
     Scaffold(
-        topBar = {
-            TopAppBar(
-                modifier= Modifier.fillMaxWidth()) {
-                Row(
-                    Modifier.fillMaxSize(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        maxLines = 1,
-                        text = "TaskIt",
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-            } },
-        content = {TwoButtonRow();DropdownList()},
-        bottomBar = { BottomAppBar{Text(text="Navigation menu")}},
+        topBar = { MainTopBar(title = "Taskit", navController = navController )},
+        content = {
+            Column {
+                TwoButtonRow();
+                DropdownList()
+            }
+        },
+        bottomBar = {BottomNavigationBar()}
     )
 }
 
@@ -143,6 +187,118 @@ fun DropdownList(){
         }
     }
 }
+
+@Composable
+fun BottomNavigationBar() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Button(
+            onClick = { /* ... */ },
+        ) {
+            Icon(
+                Icons.Filled.Send,
+                contentDescription = "Chat",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+        }
+        Button(
+            onClick = { /* ... */ },
+        ) {
+            Icon(
+                Icons.Filled.AccountCircle,
+                contentDescription = "Account",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+        }
+        Button(
+            onClick = { /* ... */ },
+        ) {
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Favorite",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun InfoScreen(navController: NavController){
+    Scaffold(
+        topBar = { ScreenTopBar("Info", navController)},
+        content = { Text(text="Content for Info Screen")},
+    )
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun SettingsScreen(navController: NavController){
+    Scaffold(
+        topBar = { ScreenTopBar(title = "Settings", navController = navController)},
+        content = { Text(text = "Content for Settings screen")},
+    )
+}
+
+/*
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MyApp() {
+    var expanded by remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier= Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        textAlign = TextAlign.Center,
+                        maxLines = 1,
+                        text = "TaskIt",
+                        fontWeight = FontWeight.Bold
+                    )
+                    // Add an IconButton for the dropdown menu
+                    IconButton(onClick = { expanded = true }) {
+                        Icon(Icons.Default.MoreVert, contentDescription = "Menu")
+                    }
+                    // Create a DropdownMenu with items
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        DropdownMenuItem(onClick = {
+                            // Do something when the first item is clicked
+                            expanded = false
+                        }) {
+                            Text("Info")
+                        }
+                        DropdownMenuItem(onClick = {
+                            // Do something when the second item is clicked
+                            expanded = false
+                        }) {
+                            Text("Settings")
+                        }
+                    }
+                }
+            }
+        },
+        content = {Column{
+            TwoButtonRow();
+            DropdownList()
+        }},
+        bottomBar = { BottomAppBar{
+            BottomNavigationBar()
+        }},
+    )
+}
+*/
+
 
 @Preview(showBackground = true)
 @Composable
